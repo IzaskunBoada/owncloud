9403fc20146bb00355c47d6edbbb776835241fa4
root <root@g11.com>
2017-10-27 11:36:42 +0000

    first commit

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oco1i4482cfu',
+  'passwordsalt' => '1lop4Nttc4A3OxgX9fRSygfU2sOYor',
+  'secret' => 'QGq0zM/pwXp5DtzVijQElFqIawneEW4kVazE5oJTi/oDSLzT',
+  'trusted_domains' => 
+  array (
+    0 => '10.9.53.104',
+  ),
+  'datadirectory' => '/var/www/html/owncloud/data',
+  'overwrite.cli.url' => 'http://10.9.53.104/owncloud',
+  'dbtype' => 'mysql',
+  'version' => '10.0.3.3',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'owncloud',
+  'dbpassword' => 'admingrupo1',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
